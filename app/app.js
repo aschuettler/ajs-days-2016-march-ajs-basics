95a6847b643a29180ed941a0a94f88de7440cd2c
Elmar Burke <elmar.burke@symetics.com>
2016-03-21 12:06:23 +0100

    colorpickersIndex

--- app/app.js
+++ app/app.js
@@ -1,5 +1,17 @@
 'use strict'
 
 angular.module('myApp', [
+  // external Modules
+  'ngRoute',
+  // internal Modules
   'myApp.colorpicker'
 ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        template: '<h1>It works</h1>'
+      })
+      .when('/sure', {
+        template: '<h1>Yes, really!</h1>'
+      })
+  })
---
+++ app/components/colorpickers-index/colorpickers-index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+angular.module('myApp.colorpickers-index', [])
+  // <colorpickers-index></colorpickers-index>
+  .component('colorpickersIndex', {
+    // Ja, das geht. Sollte aber seperat dadrüber stehen.
+    controller: function () {
+      this.red = 255
+      this.green = 0
+      this.blue = 0
+      this.alpha = 0.5
+    },
+    templateUrl: 'components/colorpickers-index/colorpickers-index.js'
+  })
